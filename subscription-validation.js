83aba909d53a0d164e6910bd2eb882d5b4b72e2d
Patricio Iwanowski <patoiwa@gmail.com>
2020-11-01 18:59:00 -0300

    Final Validation and Subscription Button at the end of index.html

--- subscription-validation.js
+++ subscription-validation.js
@@ -1,6 +1,7 @@
 //Global Variables
 arrayLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'];
 arrayNumbers = [0,1,2,3,4,5,6,7,8,9];
+let submitErrors = [];
 
 // Get Elements
 var nameVar = document.myform.name;
@@ -60,125 +61,154 @@ function errorMessage (target, error) {
 function removeMessage(e){
     e.target.nextElementSibling.remove()
 }
+function checkGlobalErrors (errorM){
+    counterOfMessages = 0;    
+    for (i=0 ; i<= (submitErrors.length-1); i++){
+        if (submitErrors[i].indexOf(errorM) !== -1){
+            counterOfMessages += 1;
+        }
+    }
+    if (counterOfMessages === 0){
+        submitErrors.push(errorM)
+    }
+}
+function removeGlobalErrors (errorM){ 
+    for (i=0 ; i<= (submitErrors.length-1); i++){
+        if (submitErrors[i].indexOf(errorM) !== -1){
+            submitErrors.splice(i,1);
+        }
+    }
+}
 
 
 //Validate Functions
 function validateName(e){
+    errorM = " Error: the name must contain at least 6 leters and have an space ";
     if ( (checkLength(e.target.value, 7, 80)) && (checkCharacters(e.target.value, [" "], true)) && (checkCharacters(e.target.value,arrayNumbers,false))){
         //console.log("no error");  Tester
         errorMessage(e.target, "");
-        return true;
+        removeGlobalErrors(errorM);
     }else{
-        //console.log("Error"); Tester 
-        errorM = " Error: the name must contain at least 6 leters and have an space ";
+        //console.log("Error"); Tester    
         errorMessage(e.target, errorM);
-        return false;
-    }  
+        checkGlobalErrors(errorM);
+    }
+    console.log(submitErrors);  
 }
 function validatePass (e){
+    errorM = " Error: the password must contain at least 8 characters: leters and numbers ";
     if(((checkLength(e.target.value,8,16))&&(checkCharactersNotAll(e.target.value,arrayLetters))) && checkCharactersNotAll(e.target.value, arrayNumbers)){
         //console.log('no error'); Tester
         errorMessage(e.target, "");
-        return true;
+        removeGlobalErrors(errorM);
     }else{
-        errorM = " Error: the password must contain at least 8 characters: leters and numbers ";
         errorMessage(e.target, errorM);
-        return false;
+        checkGlobalErrors(errorM);
     }
+    console.log(submitErrors); 
 }
 function validateAge(e){
+    errorM = " Error: you must be over 18 years old to subscribe";
     if ((parseInt(e.target.value, 10) >= 18)  && (checkCharacters(e.target.value, arrayLetters, false))){
         //console.log("no error");  Tester
         errorMessage(e.target, "");
-        return true;
+        removeGlobalErrors(errorM);
     }else{
         //console.log("Error"); Tester 
-        errorM = " Error: you must be over 18 years old to subscribe";
         errorMessage(e.target, errorM);
-        return false;
+        checkGlobalErrors(errorM);
     } 
+    console.log(submitErrors); 
 }
 function validatePhone(e){
+    errorM = " Error: the phone number must be at least 7 digits and not contain any symbols";
     if (checkCharacters(e.target.value, arrayLetters, false) && checkCharacters(e.target.value,[' ','-','(',')'], false) && checkLength(e.target.value,7,20)){
         //console.log("no error");  Tester
         errorMessage(e.target, "");
-        return true;
+        removeGlobalErrors(errorM);
     }else{
         //console.log("Error"); Tester 
-        errorM = " Error: the phone number must be at least 7 digits and not contain any symbols";
         errorMessage(e.target, errorM);
-        return false;
+        checkGlobalErrors(errorM);
     } 
+    console.log(submitErrors); 
 }
 function validateAdress(e){
+    errorM = " Error: the adress must contain at least 5 characters: leters, numbers and a space ";
     if (checkLength(e.target.value, 5,50) && checkCharactersNotAll(e.target.value,arrayLetters) && checkCharactersNotAll(e.target.value,arrayNumbers) && checkCharacters(e.target.value,[' '],true)){
         //console.log("no error");  Tester
         errorMessage(e.target, "");
-        return true;
+        removeGlobalErrors(errorM);
     }else{
         //console.log("Error"); Tester 
-        errorM = " Error: the adress must contain at least 5 characters: leters, numbers and a space ";
         errorMessage(e.target, errorM);
-        return false;
+        checkGlobalErrors(errorM);
     } 
+    console.log(submitErrors); 
 }
 function validateCityAndPostal(e){
+    errorM = " Error: City or Postal Code must have at leats 3 characters ";
     if (checkLength(e.target.value,3,50)){
         //console.log("no error");  Tester
         errorMessage(e.target, "");
-        return true;
+        removeGlobalErrors(errorM);
     }else{
         //console.log("Error"); Tester 
-        errorM = " Error: must have at leats 3 characters ";
         errorMessage(e.target, errorM);
-        return false;
+        checkGlobalErrors(errorM);
     } 
+    console.log(submitErrors); 
 }
 function validateDni(e){
+    errorM = " Error: DNI must contain between 7 and 8 numbers ";
     if (checkLength(e.target.value,7,8) && checkCharacters(e.target.value,arrayLetters,false)){
         //console.log("no error");  Tester
         errorMessage(e.target, "");
-        return true;
+        removeGlobalErrors(errorM);
     }else{
         //console.log("Error"); Tester 
-        errorM = " Error: DNI must contain between 7 and 8 numbers ";
         errorMessage(e.target, errorM);
-        return false;
+        checkGlobalErrors(errorM);
     } 
+    console.log(submitErrors); 
 }
 function validateEmail(e){
+    errorM = " Error: must be a valid email format ";
     if (checkCharacters(e.target.value,['@','.com'],true)){
         //console.log("no error");  Tester
         errorMessage(e.target, "");
-        return true;
+        removeGlobalErrors(errorM);
     }else{
         //console.log("Error"); Tester 
-        errorM = " Error: must be a valid email format ";
         errorMessage(e.target, errorM);
-        return false;
+        checkGlobalErrors(errorM);
     } 
+    console.log(submitErrors); 
 }
 function validateEnter(e){
     e.preventDefault();
-    alert('default prevented');
-    if(enter === true){
-        alert('no error');
-    }else if (enter === false){
-        alert('error');
+    if (nameVar.value === '' || password.value === '' || age.value === '' || phoneNumber.value === '' || adress.value === '' || city.value === '' || postalCode.value === '' || dni.value === '' || email.value === '') {
+        alert('Error: you must fill all camps');
+    }else if (submitErrors.length === 0){
+        alert('You have submited: Name:'+ nameVar.value+
+        " Password: "+ password.value+" Age: "+age.value+" Phone Number: "+ phoneNumber.value+ " Adress; "+ adress.value+ "City: "+ city.value+" Postal Code: " + postalCode.value+" DNI: "+ dni.value+" Email: "+email.value);
+    }else {
+        alert(submitErrors);
     }
+    console.log(submitErrors); 
 }
 
 //Event Listeners
 //Blur
-enter = nameValidation = nameVar.addEventListener('blur', validateName);
-enter = password.addEventListener('blur', validatePass);
-enter = age.addEventListener('blur', validateAge);
-enter = phoneNumber.addEventListener('blur', validatePhone);
-enter = adress.addEventListener('blur', validateAdress);
-enter = city.addEventListener('blur', validateCityAndPostal);
-enter = postalCode.addEventListener('blur', validateCityAndPostal);
-enter = dni.addEventListener('blur', validateDni);
-enter = email.addEventListener('blur', validateEmail);
+nameVar.addEventListener('blur', validateName);
+password.addEventListener('blur', validatePass);
+age.addEventListener('blur', validateAge);
+phoneNumber.addEventListener('blur', validatePhone);
+adress.addEventListener('blur', validateAdress);
+city.addEventListener('blur', validateCityAndPostal);
+postalCode.addEventListener('blur', validateCityAndPostal);
+dni.addEventListener('blur', validateDni);
+email.addEventListener('blur', validateEmail);
 
  
 //Focus
@@ -193,4 +223,4 @@ dni.addEventListener('focus', removeMessage);
 email.addEventListener('focus', removeMessage);
 
 //Click
-subsBtn.addEventListener('click', validateEnter);
+subsBtn.onclick = validateEnter;
